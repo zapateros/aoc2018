aa5f53b07647bb7a98b80c4ba0ef697d0d10e36e
Paul <plschoenmakers@hotmail.com>
2019-01-11 15:47:37 +0100

    Update day_01.R

--- Solutions/day_01.R
+++ Solutions/day_01.R
@@ -4,6 +4,6 @@ sum(t)
 
 # 1.2 ----
 t   <- read.table("input_day_1.txt")
-rp  <- cumsum(rep(t(t),500))
-dup <- which(duplicated(rp)==TRUE)
+rp  <- cumsum(rep(t(t), 500))
+dup <- which(duplicated(rp) == TRUE)
 rp[min(dup)]
