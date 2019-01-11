a59486cae40fa3aa50c3fc67b4b46b2ccceaaaf6
Paul <plschoenmakers@hotmail.com>
2019-01-11 15:47:14 +0100

    Update day_02.R

--- Solutions/day_02.R
+++ Solutions/day_02.R
@@ -8,23 +8,23 @@ for(i in 1:nrow(t)){
   if(2 %in% tb){two   <- two + 1}
   if(3 %in% tb){three <- three + 1}
 }
-two*three
+two * three
 
 
 # 2.2 ----
 t <- read.table("input_day_2.txt")
 for(i in 1:nrow(t)){
-  ch1 <- unlist(strsplit(as.character(t$V1[i]),""))
+  ch1 <- unlist(strsplit(as.character(t$V1[i]), ""))
   for(j in (i + 1):nrow(t)){
-    ch2 <- unlist(strsplit(as.character(t$V1[j]),""))
-    df  <- sum(ch1!=ch2)
+    ch2 <- unlist(strsplit(as.character(t$V1[j]), ""))
+    df  <- sum(ch1 != ch2)
     if(df == 1){
       break
     }
   }
   if(df == 1){
-    tg <- paste(ch1[ch1==ch2],collapse = "")
-    cat(i," ",j," ",tg)
+    tg <- paste(ch1[ch1 == ch2], collapse = "")
+    cat(i, " ", j," ", tg)
     break
   }
 }
