503eecedbb89b48c6ec873d8afdcbd3381387ec5
Paul <plschoenmakers@hotmail.com>
2019-01-11 15:43:11 +0100

    Clean up a little bit

--- Solutions/day_05.R
+++ Solutions/day_05.R
@@ -1,54 +1,48 @@
 # 5.1 ----
-t<-readLines("input_day_5.txt")
-cmb<-c(paste0(letters,LETTERS),paste0(LETTERS,letters),"opt")
+t   <- readLines("input_day_5.txt")
+cmb <- c(paste0(letters, LETTERS), paste0(LETTERS, letters), "opt")
 
-ref<-0
-while(ref==0){
-  occ<-sapply(cmb,function(x){
-    regexpr(x,t)[1]
+ref <- 0
+while(ref == 0){
+  occ <- sapply(cmb, function(x){
+    regexpr(x, t)[1]
   })
-  
-  occ<-occ[-(which(occ== -1))]
-  
-  if(length(occ)>0){
-    ltr<-names(occ[occ==min(occ)])
-    t<-sub(ltr,"",t)
+  occ <- occ[-(which(occ == -1))]
+  if(length(occ) > 0){
+    ltr <- names(occ[occ == min(occ)])
+    t   <- sub(ltr, "", t)
   }else{
-    ref<-1
+    ref <- 1
   }
 }
-
 nchar(t)
 
-
 # 5.2 ----
-t<-readLines("input_day_5.txt")
-cmb<-c(paste0(letters,LETTERS),paste0(LETTERS,letters),"opt")
-rst<-t
+t   <- readLines("input_day_5.txt")
+cmb <- c(paste0(letters, LETTERS), paste0(LETTERS, letters), "opt")
+rst <- t
 
-lng<-NULL
+lng <- NULL
 for(i in 1:length(letters)){
-  t<-rst
-  t<-gsub(lwr[i],"",t)
-  t<-gsub(upr[i],"",t)
+  t <- rst
+  t <- gsub(lwr[i], "", t)
+  t <- gsub(upr[i], "", t)
   
   ref<-0
-  while(ref==0){
-    occ<-sapply(cmb,function(x){
-      regexpr(x,t)[1]
+  while(ref == 0){
+    occ <- sapply(cmb, function(x){
+      regexpr(x, t)[1]
     })
-    
-    occr<-occ[-(which(occ== -1))]
-    if(length(occr)>0){
-      ltr<-names(occr[occr==min(occr)])
-      t<-sub(ltr,"",t)
+    occr <- occ[-(which(occ == -1))]
+    if(length(occr) > 0){
+      ltr <- names(occr[occr == min(occr)])
+      t   <-sub(ltr, "", t)
     }else{
-      ref<-1
+      ref <- 1
     }
   }
-  
-  ch<-nchar(t)
-  lng<-c(lng,ch)
+  ch  <- nchar(t)
+  lng <- c(lng, ch)
 }
 
 min(lng)
